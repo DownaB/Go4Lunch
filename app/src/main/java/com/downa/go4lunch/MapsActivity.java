130ea8a13dd6dd88ec600fdcdfff0f4517d53075
DownaB <61759222+DownaB@users.noreply.github.com>
2021-05-19 20:18:39 +0200

    activity-main + activity-maps

---
+++ app/src/main/java/com/downa/go4lunch/MapsActivity.java
@@ -0,0 +1,42 @@
+package com.downa.go4lunch;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.downa.go4lunch.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        map = googleMap;
+
+        LatLng paris = new LatLng(48.96314, 2.37563);
+        map.addMarker(new MarkerOptions().position(paris).title("Marker in Paris"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(paris));
+    }
+}
