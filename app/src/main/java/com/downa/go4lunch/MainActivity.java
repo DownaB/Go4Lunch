f03f572ddf9d585463b7b02c58a3d2baae6fb73a
DownaB <61759222+DownaB@users.noreply.github.com>
2021-05-31 23:01:02 +0200

    auth google FB

--- app/src/main/java/com/downa/go4lunch/MainActivity.java
+++ app/src/main/java/com/downa/go4lunch/MainActivity.java
@@ -8,8 +8,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.downa.go4lunch.databinding.ActivityMainBinding;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -21,12 +28,15 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -35,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth fireBaseAuth;
     private GoogleSignInClient googleSignInClient;
     int RC_SIGN_IN = 0;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,19 +55,35 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        fireBaseAuth = FirebaseAuth.getInstance();
+
         SignInButton signInButton = binding.signInButtonGoogle;
         signInButton.setSize(SignInButton.SIZE_STANDARD);
 
-        binding.signInButtonGoogle.setOnClickListener(new View.OnClickListener() {
+        callbackManager = CallbackManager.Factory.create();
+        binding.signInButtonFacebook.setReadPermissions("email", "public_profile");
+        binding.signInButtonFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View view) {
-                switch (view.getId()) {
-                    case binding.signInButtonGoogle:
-                        signIn();
-                        break;
-                }
+            public void onSuccess(LoginResult loginResult) {
+              Log.d(TAG,"facebook:onSuccess" + loginResult);
+              handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG,"facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG,"facebook:onError", error);
             }
         });
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -65,41 +92,86 @@ public class MainActivity extends AppCompatActivity {
 
         googleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        fireBaseAuth = FirebaseAuth.getInstance();
+        binding.signInButtonGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case binding.signInButtonGoogle:
+                        signIn();
+                        break;
+                }
+            }
+        });
 
     }
-        private void signIn(){
 
-            Intent signInIntent = googleSignInClient.getSignInIntent();
-            startActivityForResult(signInIntent, RC_SIGN_IN);
-        }
+    private void signIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
         @Override
         protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
             super.onActivityResult(requestCode, resultCode, data);
 
-            if (requestCode == RC_SIGN_IN){
+            if (requestCode == RC_SIGN_IN) {
                 Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-               handleSignInResult(task);
-            }
-
-            private void handleSignInResult(Task<GoogleSignInAccount> completedTask){
-                try{
-                    GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-                    updateUI(account);
-                }catch (ApiException e){
-                    Log.w(TAG, "signInResult : failed code=" + e.getStatusCode());
-                    updateUI(null);
+                try {
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                    firebaseAuthWithGoogle(account.getIdToken());
+                } catch (ApiException e) {
+                    Log.w(TAG, "Google sign in failed", e);
                 }
             }
         }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+        @Override
+        public void onStart() {
+            super.onStart();
+            FirebaseUser currentUser = fireBaseAuth.getCurrentUser();
+            updateUI(currentUser);
+    }
 
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        fireBaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = fireBaseAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        fireBaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = fireBaseAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
     }
 
 }
